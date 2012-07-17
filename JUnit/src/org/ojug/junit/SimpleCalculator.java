9e358dbffb1a44edb28891f246052944589adcaa
pheaber <paul@heaberlin.net>
2012-07-17 10:50:45 -0500

    add Parameterized example

---
+++ JUnit/src/org/ojug/junit/SimpleCalculator.java
@@ -0,0 +1,9 @@
+package org.ojug.junit;
+
+public class SimpleCalculator {
+
+    public int add(int addendOne, int addendTwo) {
+        return addendOne + addendTwo;
+    }
+
+}
---
+++ JUnit/tests/org/ojug/junit/ParameterizedExamples.java
@@ -0,0 +1,43 @@
+package org.ojug.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ParameterizedExamples {
+
+    private final int addendOne;
+    private final int addendTwo;
+
+    private final int explectedResult;
+
+    private final SimpleCalculator calculator = new SimpleCalculator();
+
+    public ParameterizedExamples(int addendOne, int addendTwo, int expectedResult) {
+        this.addendOne = addendOne;
+        this.addendTwo = addendTwo;
+        this.explectedResult = expectedResult;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] { //
+                { 1, 1, 2 }, //
+                        { 2, 6, 8 }, //
+                        { 9, 3, 12 } //
+                });
+    }
+
+    @Test
+    public void add() {
+        assertEquals(this.explectedResult, this.calculator.add(this.addendOne, this.addendTwo));
+    }
+
+}
